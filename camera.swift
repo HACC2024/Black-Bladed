23047d4dc537a78cd4df477c0748cb5afb30e740
ElijahTapat18 <161895884+ElijahTapat18@users.noreply.github.com>
2024-11-08 11:55:40 -1000

    camera testing

--- camera.swift
+++ camera.swift
@@ -1,4 +1,24 @@
-let imagePickerController = UIImagePickerController()
-imagePickerController.delegate = self
-imagePickerController.sourceType = .camera
-present(imagePickerController, animated: true, completion: nil)
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let imagePickerController = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+    }
+
+    @IBAction func takePicture(_ sender: UIButton) {
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let image = info[.originalImage] as? UIImage {
+            // Use the captured image
+            print("Captured image: \(image)")
+        }
+    }
+}
