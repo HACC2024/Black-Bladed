f21e96bd0c7c7effa9c93871c295832c6d0eb964
ElijahTapat18 <161895884+ElijahTapat18@users.noreply.github.com>
2024-11-08 11:50:31 -1000

    camera testing

--- camera.swift
+++ camera.swift
@@ -1,1 +1,25 @@
+// Create a button in your storyboard or view file
+// @IBOutlet weak var captureButton: UIButton!
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var captureButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captureButton.addTarget(self, action: #selector(captureImage), for: .touchUpInside)
+    }
+
+    @objc func captureImage() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let image = info[.originalImage] as? UIImage {
+            // Do something with the captured image
+        }
+    }
+}
